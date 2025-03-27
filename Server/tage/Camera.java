eed99f3573a14aacd5a8449d9df731c8acc26231
tuandoancsus <tuandoan@csus.edu>
2025-03-27 15:02:30 -0700

    Commit
    Moved TAGE to server and client

---
+++ Server/tage/Camera.java
@@ -0,0 +1,199 @@
+package tage;
+import java.lang.Math;
+
+import org.joml.*;
+
+import net.java.games.input.Event;
+import tage.input.action.AbstractInputAction;
+
+/**
+* A Camera specifies the rendering viewpoint for a Viewport.
+* A Camera instance includes fields for location, and U, V, N vectors.
+* It includes look-at() methods for pointing the camera at a given location or object.
+* It also includes a method for generating a VIEW matrix.
+* <p>
+* U, V, and N must form an orthogonal left-handed system of axes indicating the camera orientation.
+* Note that this is NOT a camera controller - however, a controller could be written
+* for a Camera instance by modifying location, U, V, and N based on user input.
+* The default camera position is at (0,0,1) looking down the -Z axis towards the origin.
+* @author Scott Gordon
+*/
+
+public class Camera
+{
+	private Vector3f u, v, n;
+	private Vector3f defaultU, defaultV, defaultN;
+	private Vector3f location, defaultLocation;
+	private Matrix4f view, viewR, viewT;
+
+	/** instantiates a Camera object at location (0,0,1) and facing down the -Z axis towards the origin */
+	public Camera()
+	{	defaultLocation = new Vector3f(0.0f, 0.0f, 1.0f);
+		defaultU = new Vector3f(1.0f, 0.0f, 0.0f);
+		defaultV = new Vector3f(0.0f, 1.0f, 0.0f);
+		defaultN = new Vector3f(0.0f, 0.0f, -1.0f);
+		location = new Vector3f(defaultLocation);
+		u = new Vector3f(defaultU);
+		v = new Vector3f(defaultV);
+		n = new Vector3f(defaultN);
+		view = new Matrix4f();
+		viewR = new Matrix4f();
+		viewT = new Matrix4f();
+	}
+
+	/** sets the world location of this Camera */
+	public void setLocation(Vector3f l) { location.set(l); }
+
+	/** sets the U (right-facing) vector for this Camera */
+	public void setU(Vector3f newU) { u.set(newU); }
+
+	/** sets the V (upward-facing) vector for this Camera */
+	public void setV(Vector3f newV) { v.set(newV); }
+
+	/** sets the N (forward-facing) vector for this Camera */
+	public void setN(Vector3f newN) { n.set(newN); }
+
+	/** returns the world location of this Camera */
+	public Vector3f getLocation() { return new Vector3f(location); }
+
+	/** gets the U (right-facing) vector for this Camera */
+	public Vector3f getU() { return new Vector3f(u); }
+
+	/** gets the V (upward-facing) vector for this Camera */
+	public Vector3f getV() { return new Vector3f(v); }
+
+	/** gets the N (forward-facing) vector for this Camera */
+	public Vector3f getN() { return new Vector3f(n); }
+
+
+	/** orients this Camera so that it faces a specified Vector3f world location */
+	public void lookAt(Vector3f target) { lookAt(target.x(), target.y(), target.z()); }
+
+	/** orients this Camera so that it faces a specified GameObject */
+	public void lookAt(GameObject go) { lookAt(go.getWorldLocation()); }
+
+	/** orients this Camera so that it faces a specified (x,y,z) world location */
+	public void lookAt(float x, float y, float z)
+	{	setN((new Vector3f(x-location.x(), y-location.y(), z-location.z())).normalize());
+		Vector3f copyN = new Vector3f(n);
+		if ((n.equals(0,1,0)) || (n.equals(0,-1,0)))
+			u = new Vector3f(1f,0f,0f);
+		else
+			u = (new Vector3f(copyN.cross(0f,1f,0f))).normalize();
+		Vector3f copyU = new Vector3f(u);
+		v = (new Vector3f(copyU.cross(n))).normalize();
+	}
+
+	protected Matrix4f getViewMatrix()
+	{	viewT.set(1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		-location.x(), -location.y(), -location.z(), 1.0f);
+
+		viewR.set(u.x(), v.x(), -n.x(), 0.0f,
+		u.y(), v.y(), -n.y(), 0.0f,
+		u.z(), v.z(), -n.z(), 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f);
+
+		view.identity();
+		view.mul(viewR);
+		view.mul(viewT);
+
+		return(view);
+	}
+
+	// --------------- Movement ---------------- 
+	/**
+	 * Moves the object forward in the direction it's facing.
+	 *
+	 * @param direction The amount to move forward.  A positive value moves the object forward,
+	 *                  a negative value moves it backward.
+	 */
+	public void fwdAction(float direction) {
+		Vector3f fwdDirection = new Vector3f(getN().mul(direction));
+		fwdDirection.mul(0.1f);
+		setLocation(getLocation().add(fwdDirection.x(), fwdDirection.y(), fwdDirection.z()));
+	}
+
+	/**
+	 * Rotates the object around its vertical (Y) axis.
+	 *
+	 * @param angle The angle of rotation in radians.  A positive value rotates the object
+	 *              clockwise (when viewed from above), a negative value rotates it counter-clockwise.
+	 */
+	public void yaw(float angle) {
+		Vector3f rightVector = new Vector3f(getU());
+		Vector3f upVector = new Vector3f(getV());
+		Vector3f fwdVector = new Vector3f(getN());
+
+		rightVector.rotateAxis(angle, upVector.x(), upVector.y(), upVector.z());
+		fwdVector.rotateAxis(angle, upVector.x(), upVector.y(), upVector.z());
+
+		setU(rightVector);
+		setN(fwdVector);
+	}
+
+	/**
+	 * Rotates the object around its horizontal (X) axis (pitch).
+	 *
+	 * @param angle The angle of rotation in radians. A positive value rotates the object upwards,
+	 *             a negative value rotates it downwards.
+	 */
+	public void pitch(float angle) {
+		Vector3f rightVector = new Vector3f(getU());
+		Vector3f upVector = new Vector3f(getV());
+		Vector3f fwdVector = new Vector3f(getN());
+
+		upVector.rotateAxis(angle, rightVector.x(), rightVector.y(), rightVector.z());
+		fwdVector.rotateAxis(angle, rightVector.x(), rightVector.y(), rightVector.z());
+
+		setV(upVector);
+		setN(fwdVector);
+	}
+
+	/**
+	 * Rotates the object around its forward (Z) axis (roll).
+	 *
+	 * @param angle The angle of rotation in radians. A positive value rotates the object clockwise
+	 *             when looking along the forward vector, a negative value rotates it counter-clockwise.
+	 */
+	public void roll(float angle) {
+		Vector3f rightVector = new Vector3f(getU());
+		Vector3f upVector = new Vector3f(getV());
+		Vector3f fwdVector = new Vector3f(getN());
+
+		rightVector.rotateAxis(angle, fwdVector.x(), fwdVector.y(), fwdVector.z());
+		upVector.rotateAxis(angle, fwdVector.x(), fwdVector.y(), fwdVector.z());
+
+		setU(rightVector);
+		setV(upVector);
+	}
+
+	public void zoom(float zoomAmount) { 
+		Vector3f newPosition = getLocation().add(getN().mul(zoomAmount * 0.1f));
+		setLocation(newPosition);
+	}
+
+	public void panVertical(float amount) {
+		// Get the current location of the camera
+		Vector3f currentPosition = getLocation();
+		
+		// Adjust the Y coordinate for vertical panning (up or down)
+		Vector3f newPosition = currentPosition.add(0, 0, amount);
+		
+		// Set the new location of the camera
+		setLocation(newPosition);
+	}
+	
+	public void panHorizontal(float amount) {
+		// Get the current location of the camera
+		Vector3f currentPosition = getLocation();
+		
+		// Adjust the X coordinate for horizontal panning (left or right)
+		Vector3f newPosition = currentPosition.add(amount, 0, 0);
+		
+		// Set the new location of the camera
+		setLocation(newPosition);
+	}
+	
+}
